a79cc4b89e6194555e6144d3b2778eef6cfb68d0
Lucy Mo <lululucyyyyyyy@Lucys-MacBook-Pro-2.local>
2019-07-30 13:31:30 -0400

    upload raspberry pi demo

---
+++ pi_demo.py
@@ -0,0 +1,33 @@
+import subprocess
+from PIL import Image
+import numpy as np
+import tensorflow as tf
+import cv2
+import time
+
+interpreter = tf.lite.Interpreter(model_path='/home/pi/the_tflite.tflite')
+interpreter.allocate_tensors()
+
+input_details = interpreter.get_input_details()
+output_details = interpreter.get_output_details()
+
+input_shape = input_details[0]['shape']
+
+while True:
+	subprocess.call("ifswebcam -D 0.01 --no-banner --quiet img.jpeg", shell=True)
+	img = Image.open("img.jpeg")
+	input_data = np.expand_dims(np.array(img.resize((224, 224)), dtype=np.float32), 0)
+
+	interpreter.set_tensor(input_details[0]['index'], input_data) 
+
+	toc = time.time()
+	interpreter.invoke()
+	tic = time.time()
+	the_time = tic - toc
+
+	output_data = interpreter.get_tensor(output_details[0]['index'])
+	answer = np.argmax(output_data)
+	if output == 1:
+		print('one')
+	else:
+		print('five')
